84503574a1ef9a10cf5ac771bd5b2ac240f12b87
KevChausse <chaussekevin@gmail.com>
2017-08-01 14:30:47 +0200

    Creation d'une premiere route pour l'api: GET /users/ qui renvoie la liste des users

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,9 +1,32 @@
 var express = require('express');
 var router = express.Router();
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+	host : 'localhost', // à renseigner
+	user : 'root', // à renseigner
+	password : '', // à renseigner
+	database : 'gestbudget_db' // à renseigner
+});
+
+
+connection.connect();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  
+  var getUsers = function(retFunc){
+    var query = "SELECT login_connect, password_connect FROM connect";
+    connection.query(query,function(error, results, fields) {
+        retFunc(results);
+    }); 
+  }
+  getUsers(function(results) {
+    res.json(results);
+  });
+  
+
+
 });
 
 module.exports = router;
